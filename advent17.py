03fc5d44a68efc08c067444588a2aa8f2453a75f
Christian <crille.89@gmail.com>
2015-12-17 11:28:26 +0100

    day17

---
+++ advent17.py
@@ -0,0 +1,21 @@
+s = 0
+
+def combinations(m, con, used):
+	n = 0
+	for i in range(len(con)):
+		if con[i] < m:
+			if i < len(con)-1:
+				n += combinations(m-con[i], con[1+i-len(con):], used + [con[i]])
+		elif con[i] == m:
+			if len(used) < 4:
+				global s
+				s += 1
+#				print(len(used)+1, used+[con[i]], s)
+			n += 1
+	return n
+
+#ctest = [20,15,10,5,5]
+containers = [50,44,11,49,42,46,18,32,26,40,21,7,18,43,10,47,36,24,22,40]
+
+print(combinations(150, containers, []), s)
+#print(combinations(25, ctest, []))
