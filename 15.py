7835f3baa2eaebaf7cefd737307d677e9a854aae
Christian <crille.89@gmail.com>
2016-12-15 09:27:55 +0100

    Day 15

--- 15.py
+++ 15.py
@@ -1,10 +1,8 @@
-discs = [(17,5),(19,8),(7,1),(13,7),(5,1),(3,0),(11,0)]
-st = 0
-while True:
-    t = st
-    for p,s in discs:
-        t += 1
-        if (s+t)%p: break
-    else: break
-    st += 1
-print(st)
+#l = [l.split() for l in open('15.in').readlines()]
+#discs = [(int(l[i][3]),int(l[i][-1][:-1]) + i+1) for i in range(len(l))]
+discs = [(101,3),(163,9),(263,13),(293,6),(373,14),(499,6),(577,7)]
+i,t = 1,0
+for p,s in discs:
+    while (s+t)%p: t += i
+    i *= p
+print(t) #60564135607891726
