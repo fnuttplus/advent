f38fd59a96faa26dd04e69615bb6093f166e6b9d
Christian <crille.89@gmail.com>
2016-12-19 08:29:40 +0100

    Day 19

--- 19.py
+++ 19.py
@@ -1,5 +1,13 @@
 print(int(bin(3001330)[3:]+'1',2))
 
+def f(x):
+    i = 1
+    while i*3 < x: i*=3
+    if x < 2*i: return x-i
+    return 2*x-3*i
+
+print(f(3001330))
+
 w = 1
 for x in range(2,3001330+1):
     if w >= x>>1: w += 1
